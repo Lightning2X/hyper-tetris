251b997823483b4fd3711b1b70e1bc5ad67c7762
Victor <v.a.vanweelden@students.uu.nl>
2017-10-06 16:48:45 +0200

    fullscreen 2

--- TetrisTemplate/TetrisGame.cs
+++ TetrisTemplate/TetrisGame.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 
 class TetrisGame : Game
 {
@@ -11,6 +13,8 @@ class TetrisGame : Game
     GameWorld gameWorld;
     protected static Point screen;
     protected Point windowSize;
+    protected Matrix spriteScale;
+
 
 
     [STAThread]
@@ -58,7 +62,28 @@ class TetrisGame : Game
             graphics.IsFullScreen = true;
             graphics.ApplyChanges();
         }
+        float targetAspectRatio = (float)screen.X / (float)screen.Y;
+        int width = graphics.PreferredBackBufferWidth;
+        int height = (int)(width / targetAspectRatio);
+        if (height > graphics.PreferredBackBufferHeight)
+        {
+            height = graphics.PreferredBackBufferHeight;
+            width = (int)(height * targetAspectRatio);
+        }
+
+        Viewport viewport = new Viewport();
+        viewport.X = (graphics.PreferredBackBufferWidth / 2) - (width / 2);
+        viewport.Y = (graphics.PreferredBackBufferHeight / 2) - (height / 2);
+        viewport.Width = width;
+        viewport.Height = height;
+        GraphicsDevice.Viewport = viewport;
+
+        inputHelper.Scale = new Vector2((float)GraphicsDevice.Viewport.Width / screen.X,
+                                        (float)GraphicsDevice.Viewport.Height / screen.Y);
+        inputHelper.Offset = new Vector2(viewport.X, viewport.Y);
+        spriteScale = Matrix.CreateScale(inputHelper.Scale.X, inputHelper.Scale.Y, 1);
     }
+
     protected override void LoadContent()
     {
         spriteBatch = new SpriteBatch(GraphicsDevice);
