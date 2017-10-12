28d47b261d9e3662c9c2ac55a875ee08f25448bc
Victor <v.a.vanweelden@students.uu.nl>
2017-10-12 14:19:08 +0200

    foutjes 2

--- TetrisTemplate/GameWorld.cs
+++ TetrisTemplate/GameWorld.cs
@@ -13,12 +13,18 @@ class GameWorld
      
       // enum for different game states (playing or game over)
       
-    enum GameState     {         Menu, Options, Playing, GameOver
-    }
+    enum GameState     {         Menu, Options, Help, Playing, GameOver    }
+    Vector2 lijnmenu = new Vector2(100, 50);
+    Vector2 lijn1 = new Vector2(100, 80);
+    Vector2 lijn2 = new Vector2(100, 110);
+    Vector2 lijn3 = new Vector2(100, 140);
+    Vector2 lijn4 = new Vector2(100, 170);
+    Vector2 lijn5 = new Vector2(100, 200);
+    Vector2 lijn6 = new Vector2(100, 230);
+    Vector2 lijn7 = new Vector2(100, 300);
 
-     
-      // screen width and height
-      
+    // screen width and height
+
     int screenWidth, screenHeight;
 
     //random number generator
@@ -50,7 +56,7 @@ class GameWorld
         screenWidth = width;
         screenHeight = height;
         random = new Random();
-        gameState = GameState.Playing;
+        gameState = GameState.Menu;
         block = Content.Load<Texture2D>("block");
         font = Content.Load<SpriteFont>("SpelFont");
         grid = new TetrisGrid(block);
@@ -68,6 +74,26 @@ class GameWorld
 
     public void Update(GameTime gameTime)
     {
+        if(gameState == GameState.Menu)
+        {
+            //zet menu! 
+        }
+        if (gameState == GameState.Options)
+        {
+            
+        }
+        if (gameState == GameState.Help)
+        {
+
+        }
+        if (gameState == GameState.GameOver)
+        {
+            
+        }
+        if (gameState == GameState.Playing)
+        {
+           
+        }
         blockfunction.NextBlock();
         Block newblock = blockfunction.RandomBlock();
     }
@@ -75,8 +101,41 @@ class GameWorld
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         grid.Draw(gameTime, spriteBatch, block);
-        DrawText("Hello!", Vector2.Zero, spriteBatch);
+        if (gameState == GameState.Options)
+        {
+
+        }
+            if (gameState == GameState.Help)
+        {
+   
+            DrawText("Help!", Vector2.Zero, spriteBatch);
+            DrawText("Press A to turn left", lijn1,spriteBatch);
+           // DrawText("press W to rotate 180 degrees", lijn3, spriteBatch);
+            DrawText("Press D to turn right", lijn2, spriteBatch);
+            DrawText("Press S to move to bottom", lijn4, spriteBatch);
+            DrawText("Press left to move left", lijn5, spriteBatch);
+            DrawText("Press right to move left", lijn6, spriteBatch);
+            DrawText("Press space to return to menu", lijn7, spriteBatch);
+        }
+        if (gameState == GameState.Menu)
+        {
+            DrawText("Menu", Vector2.Zero, spriteBatch);
+            DrawText("Start", lijn1, spriteBatch);
+            DrawText("Opties", lijn3, spriteBatch);
+            DrawText("Help en Credits", lijn5, spriteBatch);
+        }
+        if (gameState == GameState.Playing)
+        {
+       //     DrawText(Score, Vector2.Zero, spriteBatch);
+
+        }
+        if (gameState == GameState.GameOver)
+        {
+            DrawText("You lose!", lijn1, spriteBatch);
+
+        }
     }
+
      
       // utility method for drawing text on the screen
       
