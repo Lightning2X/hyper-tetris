9630b62dfc36a3e12de9fa2dd44ddd53836e323a
Tim Schagen <t.schagen@students.uu.nl>
2017-10-21 22:58:03 +0200

    Commentaar voor de Button class

--- TetrisTemplate/Button.cs
+++ TetrisTemplate/Button.cs
@@ -8,18 +8,22 @@ using Microsoft.Xna.Framework.Graphics;
 
 class Button
 {
+    // Define a button texture and a button position
     Texture2D button;
     Vector2 position;
     public Button(Texture2D button, Vector2 position)
     {
+        // assign the given button position and texture to the local variables.
         this.button = button;
         this.position = position;
-        
     }
 
+    // Checks if the mouse coördinates are inside the current button
     public bool IsClicked(InputHelper inputHelper)
     {
+        // make a rectangle called "boundingbox" around the current button
         Rectangle boundingbox = new Rectangle((int)position.X, (int)position.Y, button.Width, button.Height);
+        // checks if the mouse position is inside this box
         if (boundingbox.Contains(inputHelper.MousePosition))
         {
             return true;
@@ -28,6 +32,7 @@ class Button
             return false;
     }
 
+    // Draws the button
     public void Draw(GameTime gameTime, SpriteBatch s)
     {
         s.Draw(button, position, Color.White);
